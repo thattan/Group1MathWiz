7b59867b763173d085efd5562bcdd32ae1a5614e
Max Thomas McShane <mm709020@southeast.edu>
2021-03-08 15:28:48 -0600

    added classroom class

--- models/classRoom.php
+++ models/classRoom.php
@@ -1,8 +1,41 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class ClassRoom {
+    private $roomID, $userId, $name;
+    
+    function __construct($roomID, $userId, $name) {
+        $this->roomID = $roomID;
+        $this->userId = $userId;
+        $this->name = $name;
+    }
+    
+    function getRoomID() {
+        return $this->roomID;
+    }
 
+    function getUserId() {
+        return $this->userId;
+    }
+
+    function getName() {
+        return $this->name;
+    }
+
+    function setRoomID($roomID) {
+        $this->roomID = $roomID;
+    }
+
+    function setUserId($userId) {
+        $this->userId = $userId;
+    }
+
+    function setName($name) {
+        $this->name = $name;
+    }
+
+
+
+            
+    
+
+}
